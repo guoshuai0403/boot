6d2875a270ab474e3c2e5c9ce22b1f104e2d750e
guoshuai0403 <408054342@qq.com>
2018-08-28 15:58:55 +0800

    新建了许多项目

---
+++ wechat/subscription/src/main/java/com/simon/wechat/subscription/SubscriptionApplication.java
@@ -0,0 +1,18 @@
+package com.simon.wechat.subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description: 启动类
+ *
+ * @auth guoshuai
+ * @since 2018/8/28
+ */
+@SpringBootApplication
+public class SubscriptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionApplication.class, args);
+    }
+}
---
+++ wechat/subscription/src/main/java/com/simon/wechat/subscription/controller/MenuController.java
@@ -0,0 +1,15 @@
+package com.simon.wechat.subscription.controller;
+
+import com.simon.common.controller.BaseController;
+import org.springframework.stereotype.Controller;
+
+/**
+ * description:
+ *
+ * @auth guoshuai
+ * @since 2018/8/28
+ */
+@Controller
+public class MenuController extends BaseController {
+
+}
